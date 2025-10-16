90659fea14add70bf6e85bad89d83dd78ce961db
Shriya Sreevathsa <shriyasreevathsa510@gmail.com>
2025-10-16 12:16:35 -0700

    apogee changes

--- rocketry.java
+++ rocketry.java
@@ -1,5 +1,25 @@
 public class rocketry {
-    public static void Force() {
-    System.out.println("Hellow WOold");
+    public static double Force(double mass) {
+        double accel=9.8;
+        double force = (mass)*(accel);
+        return force;
     }
+
+    public static double Velocity(double time){
+        
+    }
+
+    public static double Apogee(double impulse, double thrustAvg, double diameter, double mass, double propMass, double currentTime){
+        double airDensity=1.225;
+        double burnTime=impulse/thrustAvg;
+        double frontalArea=(Math.PI*diameter)/4;
+        double massPerTime=mass+propMass*(1-(currentTime/burnTime));
+        double Drag=(1/2)*airDensity*0.4*frontalArea*Math.pow(velocity,2);
+        return apogee;
+
+
+
+    }
+
+
 }
